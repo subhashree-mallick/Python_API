f645b71a8b1de9ea6e93c71b77ec7956af67852e
subhashree-mallick <74946482+subhashree-mallick@users.noreply.github.com>
2021-02-15 20:48:59 -0500

    file added

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "98e2045ced6b63ba73dbf57bf1cf9493"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
-g_key = "AIzaSyBMtkr0fuPajDiW925msZi2r_X6ZP3JqHM"
+g_key = "YOUR KEY HERE!"
